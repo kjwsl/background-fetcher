20956ff37b39fc212b7c421c093d5a56fa42820c
kjwsl <larpios@protonmail.com>
2024-07-14 03:01:13 +0900

    Update: Add more prompts

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from multifetcher import WallpaperFlareImageFetcher
 
 def main():
-    fetcher = WallpaperFlareImageFetcher()
 
     query = input("Query: ")
+    thread_cnt = input("Thread Number (default=10): ")
+    if thread_cnt == "":
+        thread_cnt = 10
+    else:
+        thread_cnt = int(thread_cnt)
+    fetcher = WallpaperFlareImageFetcher(thread_cnt)
     fetcher.download_images(query)
 
 if __name__ == "__main__":
